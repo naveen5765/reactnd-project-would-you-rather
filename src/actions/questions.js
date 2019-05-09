eb43901329c89432d7c91a5173ce9351ea779c9b
Naveen Anandhan <naveenanandhan@Naveens-MacBook-Pro.local>
2019-05-10 00:00:27 +0200

    feat: Create ActionCreators for user, question and authedUser

---
+++ src/actions/questions.js
@@ -0,0 +1,54 @@
+import { saveQuestion, saveQuestionAnswer } from '../utils/api'
+
+export const RECEIVE_QUESTIONS= 'RECEIVE_QUESTIONS'
+export const ADD_QUESTION = 'ADD_QUESTION'
+export const VOTE_ANSWER = 'VOTE_ANSWER'
+
+
+export function receiveQuestions (questions) {
+  return {
+    type: RECEIVE_QUESTIONS,
+    questions,
+  }
+}
+
+function addQuestion (question) {
+  return {
+    type: ADD_QUESTION,
+    question,
+  }
+}
+
+export function handleAddQuestion (optionOneText, optionTwoText) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+
+    return saveQuestion({
+      optionOneText,
+      optionTwoText,
+      author: authedUser
+    })
+      .then((question) => dispatch(addQuestion(question)))
+  }
+}
+
+function voteAnswer (questionId, option) {
+  return {
+    type: VOTE_ANSWER,
+    questionId,
+    option,
+  }
+}
+
+export function handleVoteAnswer (questionId, answer) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+
+    return saveQuestionAnswer({
+      authedUser,
+      questionId,
+      answer
+    })
+      .then((answer) => dispatch(voteAnswer(answer)))
+  }
+}
