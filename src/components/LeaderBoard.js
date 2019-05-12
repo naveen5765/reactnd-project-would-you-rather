fbbb8f0b5971c50465f17b6f3affa919c3f7e582
Naveen Anandhan <naveenanandhan@Naveens-MacBook-Pro.local>
2019-05-12 12:42:14 +0200

    feat: Show a leaderboard based on the number of questions created and answered

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -4,21 +4,63 @@ import { Redirect } from 'react-router-dom'
 
 class LeaderBoard extends Component {
     render(){
-        let { isLoggedIn } = this.props
-        if(!isLoggedIn){
-            return <Redirect to={ '/' } />
-        }
-        return(
-            <div>
-                <p>Leader Board</p>
+      let { isLoggedIn, authedUser, users } = this.props
+
+      if(!isLoggedIn){
+          return <Redirect to={ '/' } />
+      }
+
+      let sortedUsers = Object.values(users).sort((a,b) => (Object.keys(b.answers).length + b.questions.length) - (Object.keys(a.answers).length + a.questions.length))
+      
+      return(
+          <div>
+            <br /> <br />
+            <h1 className='text-center'>Leaderboard</h1>
+            <br />
+            <div className="leaderboard mx-auto" style={{maxWidth: "70%"}}>
+              <div className="row mb-2 rounded font-weight-bold">
+                  <div className="col"></div>
+                  <div className="col">
+                      <div className="text-center">Questions</div>
+                  </div>
+                  <div className="col">
+                      <div className=" text-center">Answers</div>
+                  </div>
+                  <div className="col">
+                      <div className=" text-center">Total</div>
+                  </div>
+              </div>
+              {
+                sortedUsers.map((user) => {
+                  return (
+                    <div key={user.id} className={'row mb-2 rounded border ' + (authedUser.id === user.id ? 'border-success bd-width2' : 'border-dark')}>
+                        <div className="col align-items-center">
+                          <img src={user.avatarURL} alt="User Avatar" width="60" height="60" className="rounded mr-3" />
+                          {user.name}
+                        </div>
+                        <div className="col">
+                            <div className="text-center mt-3">{user.questions.length}</div>
+                        </div>
+                        <div className="col">
+                            <div className="text-center mt-3">{Object.keys(user.answers).length}</div>
+                        </div>
+                        <div className="col">
+                            <div className="text-center mt-3">{user.questions.length + Object.keys(user.answers).length}</div>
+                        </div>
+                    </div>
+                  )})
+              }
             </div>
+          </div>
         )
     }
 }
 
-function mapStateToProps ({ authedUser }) {
+function mapStateToProps ({ authedUser, users }) {
     return {
-        isLoggedIn: authedUser !== null
+        isLoggedIn: authedUser !== null,
+        authedUser,
+        users
     }
 }
 
