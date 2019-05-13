ef8857c8c5191ec7e741d63ce0cb0a17c237ef08
Naveen Anandhan <naveenanandhan@Naveens-MacBook-Pro.local>
2019-05-13 22:58:17 +0200

    style: Remove semi-colon at the end of import

--- src/components/Answered.NotSelectedOption.js
+++ src/components/Answered.NotSelectedOption.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from "prop-types"
 
 const NotSelectedOption = function(props){
     const { optionText, optionVoteCount, totalVoteCount, progress} = props
--- src/components/Answered.SelectedOption.js
+++ src/components/Answered.SelectedOption.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from "prop-types"
 import { IoIosDoneAll } from "react-icons/io"
 
 const SelectedOption = function(props){
